8aaf120ef4938d0f7b8a8d3ed7e810c2a12594b1
catherine <catherinearianna@gmail.com>
2023-08-13 16:00:30 -0300

    Segundo avance, permite agregar y eliminar peliculas

--- catalogo_pelis_POO.py
+++ catalogo_pelis_POO.py
@@ -1,32 +1,37 @@
 
-# importar las clases del archivo "clases.py"
+from clases import *
 
 opcion = None
 
-nombre_catalogo = input("Ingrese el nombre del catálogo: ")
+nombre_catalogo = input("Ingrese el nombre del catálogo: \n")
 catalogo = CatalogoPelicula(nombre_catalogo)  # con este paso creo mi objeto / instancia
 
 
 while opcion != 4:
-    
-    print("Opciones: ")
-    print("1. Agregar película")
-    print("2. Listar película")
-    print("3. Eliminar catálogo de películas")
-    print("4. SALIR")
-    
-    opcion = int(input("Escriba una opción del menú (del 1 al 4): "))
-
-    if opcion == 1:
-        nombre_pelicula = input("Escriba el nombre de la película: ")
-        pelicula = Pelicula(nombre_pelicula)
-        catalogo.agregar_pelicula(pelicula)
+    try:
+        print("\nOpciones: ")
+        print("1. Agregar película")
+        print("2. Listar película")
+        print("3. Eliminar catálogo de películas")
+        print("4. SALIR\n")
         
-    elif opcion == 2:
-        pass
-    elif opcion == 3:
-        pass
+        opcion = int(input("Escriba una opción del menú (del 1 al 4): "))
 
+        if opcion == 1:
+            nombre_pelicula = input("Escriba el nombre de la película: ")
+            pelicula = Pelicula(nombre_pelicula)
+            catalogo.agregar_pelicula(pelicula)
+            
+        elif opcion == 2:
+            catalogo.listar_peliculas()
+
+        
+        elif opcion == 3:
+            catalogo.eliminar_catalogo()
+
+    except Exception as e:
+        print("\n¡Ocurrio un error!\n", e, "\n")
+        opcion = None
 
 else: 
     print("Programa finalizado")    
--- clases.py
+++ clases.py
@@ -4,36 +4,35 @@ class Pelicula:
     def __init__(self, nombre):
         self.__nombre = nombre #atributo privado
 
-
-
    #crear método __str__ 
 
-
-   #crear métodos de acceso para el atributo privado: mostrar el atributo "nombre" y modificarlo
+   #Métodos de acceso para el atributo privado: mostrar el atributo "nombre" y modificarlo
    # recordar que hay dos metodos (decoradores o funciones)
-        
 
+    @property       #decorador que permite que un método sea accedido como un atributo 
+    def nombre(self): 
+        return self.__nombre
 
-
+    @nombre.setter
+    def nombre(self, nombre):
+        self.__nombre = nombre 
 
 class CatalogoPelicula:
+    
     def __init__(self, nombre):
         self.nombre = nombre
-        self.ruta_archivo = self.nombre,".txt"  #no va a ser pasado por parametro sino que lo creo a partir del nombre
+        self.ruta_archivo = f'{self.nombre}.txt'
 
-
-    def agregar_pelicula(self, pelicula):  
-        with open(self.ruta_archivo, 'a') as archivo:   # 'a' para indicar que quiero abrir el archivo
-            archivo.write(pelicula.nombre, '\n')
+    def agregar_pelicula(self, pelicula):
+        with open(self.ruta_archivo, 'a') as archivo:
+            archivo.write(f'{pelicula.nombre}\n')
 
     def listar_peliculas(self):
-        # abrir el archivo con with
-        # imprimir lo que hay en el archivo (archivo.read())
+        # abrir el archivo con with 
+        # imprimir lo que hay en el archivo ( arhivo.read() )
+        #print()
         pass
 
-    def eliminar_peliculas(self):    
+    def eliminar_catalogo(self):
         os.remove(self.ruta_archivo)
-        print("El archivo: ", self.ruta_archivo, "fue eliminado")
-        
-
-
+        print(f'Catálogo eliminado: {self.ruta_archivo}')
